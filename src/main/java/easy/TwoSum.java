b40909670d05a6926eddba829bd6aa4d46626449
ilia_solovev <ilia_solovev@epam.com>
2021-03-17 16:31:36 +0400

    TwoSum task added

---
+++ src/main/java/easy/TwoSum.java
@@ -0,0 +1,40 @@
+package easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+
+    public int[] findTwoSumByBruteForce(int[] nums, int target) {
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (i == j) {
+                    j++;
+                }
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    public int[] findTwoSumByMap(int[] nums, int target) {
+
+        Map<Integer, Integer> integerMap = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            integerMap.put(nums[i], i);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (integerMap.containsKey(target - nums[i]) && (i != integerMap.get(target - nums[i]))) {
+                return new int[] {i, integerMap.get(target - nums[i])};
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
+
+
---
+++ src/test/java/easy/TwoSumTest.java
@@ -0,0 +1,42 @@
+package easy;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class TwoSumTest {
+
+    private TwoSum twoSum;
+
+    @BeforeClass
+    public void setUp() {
+        twoSum = new TwoSum();
+    }
+
+    @Test
+    public void testTwoSumByBruteForce() {
+
+        long start = System.nanoTime();
+        int[] actualValue = twoSum.findTwoSumByBruteForce(new int[]{2,7,11,15}, 9);
+        long stop = System.nanoTime();
+        System.out.printf("%,d %n", stop - start);
+        Assert.assertEquals(actualValue, new int[]{0,1}, "Wrong answer");
+    }
+
+    @Test
+    public void testTwoSumByMap() {
+
+        long start = System.nanoTime();
+        int[] actualValue = twoSum.findTwoSumByMap(new int[]{3,2,4}, 6);
+        long stop = System.nanoTime();
+        System.out.printf("%,d %n", stop - start);
+        Assert.assertEquals(actualValue, new int[]{1,2}, "Wrong answer");
+    }
+
+    @AfterClass
+    public void tearDown() {
+        twoSum = null;
+    }
+
+}
