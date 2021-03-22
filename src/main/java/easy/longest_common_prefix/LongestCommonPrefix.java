2aef74df6a4f7a64aff00c54f86aa68b0240935f
ilia_solovev <ilia_solovev@epam.com>
2021-03-22 23:01:26 +0400

    LongestCommonPrefix done

---
+++ src/main/java/easy/longest_common_prefix/LongestCommonPrefix.java
@@ -0,0 +1,49 @@
+package easy.longest_common_prefix;
+
+//Amazon
+//Time Complexity: O(N + M) Space Complexity: O(1)
+
+
+public class LongestCommonPrefix {
+
+    /**
+     * start with the first word and cutting it if it doesn't fit others
+     */
+    public String longestCommonPrefix(String[] strs) {
+
+        if (strs.length == 0) {return "";}
+
+        String commonPrefix = strs[0];
+
+            for (int i = 1; i < strs.length; i++) {
+                while (strs[i].indexOf(commonPrefix) != 0) {
+                    commonPrefix = commonPrefix.substring(0, commonPrefix.length() - 1);
+                }
+            }
+        return commonPrefix;
+    }
+
+    /**
+     * same as the previous one but we are trying to find the shortest word to start with.
+     *
+     */
+    public String longestCommonPrefixTwo(String[] strs) {
+
+        if (strs.length == 0) {return "";}
+
+        String commonPrefix = strs[0];
+        //assuming that the prefix cannot be longer that the shortest word finding it and using as a prefix;
+        for (int i = 1; i < strs.length; i++) {
+            if (strs[i].length() < commonPrefix.length()) {
+                commonPrefix = strs[i];
+            }
+        }
+
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(commonPrefix) != 0) {
+                commonPrefix = commonPrefix.substring(0, commonPrefix.length() - 1);
+            }
+        }
+        return commonPrefix;
+    }
+}
---
+++ src/test/java/easy/longest_common_prefix/LongestCommonPrefixTest.java
@@ -0,0 +1,55 @@
+package easy.longest_common_prefix;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Share
+ * Write a function to find the longest common prefix string amongst an array of strings.
+ *
+ * If there is no common prefix, return an empty string "".
+ *
+ * Example 1:
+ *
+ * Input: strs = ["flower","flow","flight"]
+ * Output: "fl"
+ * Example 2:
+ *
+ * Input: strs = ["dog","racecar","car"]
+ * Output: ""
+ * Explanation: There is no common prefix among the input strings.
+ *
+ * Constraints:
+ *
+ * 0 <= strs.length <= 200
+ * 0 <= strs[i].length <= 200
+ * strs[i] consists of only lower-case English letters.
+ */
+
+public class LongestCommonPrefixTest {
+
+    @Test
+    public void testLongestCommonPrefixOne() {
+
+        LongestCommonPrefix longestCommonPrefix = new LongestCommonPrefix();
+        String actualValue = longestCommonPrefix.longestCommonPrefix(new String[]{"flower","flow","flight"});
+        Assert.assertEquals(actualValue, "fl", "Wrong answer");
+    }
+
+    @Test
+    public void testLongestCommonPrefixTwo() {
+
+        LongestCommonPrefix longestCommonPrefix = new LongestCommonPrefix();
+        String actualValue = longestCommonPrefix.longestCommonPrefix(new String[]{"dog","racecar","car"});
+        Assert.assertEquals(actualValue, "", "Wrong answer");
+    }
+
+    @Test
+    public void testLongestCommonPrefixMethodTwo() {
+
+        LongestCommonPrefix longestCommonPrefix = new LongestCommonPrefix();
+        String actualValue = longestCommonPrefix.longestCommonPrefixTwo(new String[]{"flower","flow","flight"});
+        Assert.assertEquals(actualValue, "fl", "Wrong answer");
+    }
+
+}
