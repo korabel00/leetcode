d252c4b19830d525a63ccc34fb9dc871d0189d17
ilia_solovev <ilia_solovev@epam.com>
2021-03-30 17:24:19 +0400

    Added MaximumSubArray task with Kadene's algorithm

---
+++ src/main/java/easy/maximum_subarray/MaximumSubArray.java
@@ -0,0 +1,52 @@
+package easy.maximum_subarray;
+
+/**
+ * Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum
+ * and return its sum.
+ *
+ * Example 1:
+ *
+ * Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+ * Output: 6
+ * Explanation: [4,-1,2,1] has the largest sum = 6.
+ * Example 2:
+ *
+ * Input: nums = [1]
+ * Output: 1
+ * Example 3:
+ *
+ * Input: nums = [5,4,-1,7,8]
+ * Output: 23
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= nums.length <= 3 * 104
+ * -105 <= nums[i] <= 105
+ *
+ *
+ * Follow up: If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+ */
+
+//Amazon
+//Microsoft
+//LinkedIn
+//Kadene's Algorithm
+public class MaximumSubArray {
+
+    public int maxSubArray(int[] nums) {
+
+        int max_sum = nums[0];
+        int current_sum = max_sum;
+
+        for (int i = 1; i < nums.length; i++) {
+            current_sum = Math.max(nums[i], current_sum + nums[i]);
+            if (current_sum > max_sum) max_sum = current_sum; // or max_sum = Math.max(current_sum, max_sum);
+        }
+        return max_sum;
+    }
+}
+//  |   nums[i]         |   -2  |   3   |   2   |   -1  |
+//  | current + nums[i] |   0   |   1   |   5   |   4   |
+//  |   current         |   -2  |   3   |   5   |   4   |
+//  |   max             |   -2  |   3   |   5   |   5   |
---
+++ src/test/java/easy/maximum_subarray/MaximumSubArrayTest.java
@@ -0,0 +1,67 @@
+package easy.maximum_subarray;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum
+ * and return its sum.
+ *
+ * Example 1:
+ *
+ * Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+ * Output: 6
+ * Explanation: [4,-1,2,1] has the largest sum = 6.
+ * Example 2:
+ *
+ * Input: nums = [1]
+ * Output: 1
+ * Example 3:
+ *
+ * Input: nums = [5,4,-1,7,8]
+ * Output: 23
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= nums.length <= 3 * 104
+ * -105 <= nums[i] <= 105
+ *
+ *
+ * Follow up: If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+ */
+
+public class MaximumSubArrayTest {
+
+    @Test
+    public void testMaxSubArrayZero() {
+        MaximumSubArray maximumSubArray = new MaximumSubArray();
+        int actual = maximumSubArray.maxSubArray(new int[]{-2,3,2,-1});
+        int expected = 5;
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testMaxSubArray() {
+        MaximumSubArray maximumSubArray = new MaximumSubArray();
+        int actual = maximumSubArray.maxSubArray(new int[]{-2,1,-3,4,-1,2,1,-5,4});
+        int expected = 6; // Explanation: [4,-1,2,1] has the largest sum = 6.
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testMaxSubArrayTwo() {
+        MaximumSubArray maximumSubArray = new MaximumSubArray();
+        int actual = maximumSubArray.maxSubArray(new int[]{1});
+        int expected = 1;
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testMaxSubArrayThree() {
+        MaximumSubArray maximumSubArray = new MaximumSubArray();
+        int actual = maximumSubArray.maxSubArray(new int[]{5,4,-1,7,8});
+        int expected = 23;
+        Assert.assertEquals(actual, expected);
+    }
+}
