69e84f845a20c656af90cabb033eeb979f5dcccf
Ilia Solovev <ilia_solovev@epam.com>
2021-05-30 23:31:24 +0400

    MaxConsecutiveOnes

---
+++ src/test/java/arrays/MaxConsecutiveOnesTest.java
@@ -0,0 +1,25 @@
+package arrays;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class MaxConsecutiveOnesTest {
+
+    @Test
+    public void testMaxConsecutiveOnesCaseOne () {
+
+        MaxConsecutiveOnes maxConsecutiveOnes = new MaxConsecutiveOnes();
+        int actual = maxConsecutiveOnes.findMaxConsecutiveOnes(new int[]{1,1,0,1,1,1});
+        int expected = 3;
+        Assert.assertEquals(actual,expected);
+    }
+
+    @Test
+    public void testMaxConsecutiveOnesCaseTwo() {
+
+        MaxConsecutiveOnes maxConsecutiveOnes = new MaxConsecutiveOnes();
+        int actual = maxConsecutiveOnes.findMaxConsecutiveOnes(new int[]{1,0,1,1,0,1});
+        int expected = 2;
+        Assert.assertEquals(actual,expected);
+    }
+}
