ff0edec76fbaf67175fac0b883ce5ad29b917839
Ilia Solovev <ilia_solovev@epam.com>
2021-05-30 22:51:39 +0400

    FindNumbersWithEvenNumberOfDigits

---
+++ src/test/java/arrays/FindNumbersWithEvenNumberOfDigitsTest.java
@@ -0,0 +1,25 @@
+package arrays;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class FindNumbersWithEvenNumberOfDigitsTest {
+
+    @Test
+    public void testEvenNumberOfDigitsCaseOne () {
+
+        FindNumbersWithEvenNumberOfDigits evenNumberOfDigits = new FindNumbersWithEvenNumberOfDigits();
+        int actual = evenNumberOfDigits.findNumbers(new int[]{12,345,2,6,7896});
+        int expected = 2;
+        Assert.assertEquals(actual,expected);
+    }
+
+    @Test
+    public void testEvenNumberOfDigitsCaseTwo() {
+
+        FindNumbersWithEvenNumberOfDigits evenNumberOfDigits = new FindNumbersWithEvenNumberOfDigits();
+        int actual = evenNumberOfDigits.findNumbers(new int[]{555,901,482,1771});
+        int expected = 1;
+        Assert.assertEquals(actual,expected);
+    }
+}
