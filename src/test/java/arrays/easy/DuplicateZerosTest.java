de83cc69c3102fed66d3a3589ca74020be2f85b6
ilia_solovev <ilia_solovev@epam.com>
2021-06-08 13:36:43 +0400

    DuplicateZeros

---
+++ src/test/java/arrays/easy/DuplicateZerosTest.java
@@ -0,0 +1,34 @@
+package arrays.easy;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DuplicateZerosTest {
+
+    @Test
+    public void testDuplicateZerosCaseOne() {
+        DuplicateZeros duplicateZeros = new DuplicateZeros();
+        int[] arrayUnderTest = {1,0,2,3,0,4,5,0};
+        duplicateZeros.duplicateZeros(arrayUnderTest);
+        int[] expected = {1,0,0,2,3,0,0,4};
+        Assert.assertArrayEquals(expected, arrayUnderTest);
+    }
+
+    @Test
+    public void testDuplicateZerosCaseTwo() {
+        DuplicateZeros duplicateZeros = new DuplicateZeros();
+        int[] arrayUnderTest = {1,2,3};
+        duplicateZeros.duplicateZeros(arrayUnderTest);
+        int[] expected = {1,2,3};
+        Assert.assertArrayEquals(expected, arrayUnderTest);
+    }
+
+    @Test
+    public void testDuplicateZerosCaseThree() {
+        DuplicateZeros duplicateZeros = new DuplicateZeros();
+        int[] arrayUnderTest = {0,0,0,0,0,0,0};
+        duplicateZeros.duplicateZeros(arrayUnderTest);
+        int[] expected = {0,0,0,0,0,0,0};
+        Assert.assertArrayEquals(expected, arrayUnderTest);
+    }
+}
