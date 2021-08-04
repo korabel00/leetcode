db5d3624c84d64c1e07988b82d09187e26ebd47e
Соловьев Илья Владимирович (4012941) <isolovev@dev.vtb.ru>
2021-08-04 13:27:26 +0400

    LinkedListCycle

---
+++ src/test/java/linked_list/easy/MyLinkedListTest.java
@@ -0,0 +1,25 @@
+package linked_list.easy;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MyLinkedListTest {
+
+    @Test
+    public void testMyLinkedListCaseOne() {
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.addAtHead(1);
+        myLinkedList.addAtTail(3);
+        myLinkedList.addAtIndex(1, 2);                          // linked list becomes 1->2->3
+        Assert.assertEquals(2,myLinkedList.get(1));               // return 2
+        myLinkedList.deleteAtIndex(1);                                    // now the linked list is 1->3
+        Assert.assertEquals(3, myLinkedList.get(1));              // return 3
+    }
+
+    @Test
+    public void testMyLinkedListCaseTwo() {
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.addAtTail(1);
+        Assert.assertEquals(1, myLinkedList.get(0));
+    }
+}
