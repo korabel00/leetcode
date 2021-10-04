b4139c2be36d32932afb8a50bce6548e3c2f7c3b
Соловьев Илья Владимирович (4012941) <isolovev@dev.vtb.ru>
2021-10-04 18:32:59 +0400

    IntersectionOfTwoArrays

---
+++ src/main/java/binary_search/IntersectionOfTwoArrays.java
@@ -0,0 +1,57 @@
+package binary_search;
+
+import java.util.HashSet;
+
+/**
+ * Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: nums1 = [1,2,2,1], nums2 = [2,2]
+ * Output: [2]
+ * Example 2:
+ *
+ * Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+ * Output: [9,4]
+ * Explanation: [4,9] is also accepted.
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= nums1.length, nums2.length <= 1000
+ * 0 <= nums1[i], nums2[i] <= 1000
+ */
+
+// easy
+// Time O(N) Space O(N)
+// Lyft Facebook
+public class IntersectionOfTwoArrays {
+
+    public int[] intersection(int[] nums1, int[] nums2) {
+
+        int minLength = Math.min(nums1.length, nums2.length);
+        HashSet<Integer> hashSet1 = new HashSet<>(minLength);
+        HashSet<Integer> intersection = new HashSet<>(minLength);
+
+        for (int i : nums1) {
+            hashSet1.add(i);
+        }
+
+        for (int i : nums2) {
+            if (hashSet1.contains(i)) {
+                intersection.add(i);
+            }
+        }
+
+        int[] result = new int[intersection.size()];
+
+        int i = 0;
+        for (Integer integer: intersection) {
+            result[i] = integer;
+            i++;
+        }
+        return result;
+    }
+}
---
+++ src/test/java/binary_search/IntersectionOfTwoArraysTest.java
@@ -0,0 +1,15 @@
+package binary_search;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class IntersectionOfTwoArraysTest {
+
+    @Test
+    public void testIntersectionOfTwoArrays() {
+
+        IntersectionOfTwoArrays intersectionOfTwoArrays = new IntersectionOfTwoArrays();
+        int[] actual = intersectionOfTwoArrays.intersection(new int[]{4,9,5}, new int[]{9,4,9,8,4});
+        Assert.assertArrayEquals(new int[]{4,9}, actual);
+    }
+}
