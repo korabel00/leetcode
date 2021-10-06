3123a3a20fd12249533bdc64a8d4b4e58c81959c
Соловьев Илья Владимирович (4012941) <isolovev@dev.vtb.ru>
2021-10-06 17:55:24 +0400

    TwoSumII

---
+++ src/test/java/binary_search/TwoSumIITest.java
@@ -0,0 +1,35 @@
+package binary_search;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TwoSumIITest {
+
+    @Test
+    public void testTwoSumIICaseOne() {
+        TwoSumII twoSumII = new TwoSumII();
+        int[] actual = twoSumII.twoSumByHashMap(new int[]{2,7,11,15}, 9);
+        Assert.assertArrayEquals(new int[]{1,2}, actual);
+    }
+
+    @Test
+    public void testTwoSumIICaseTwo() {
+        TwoSumII twoSumII = new TwoSumII();
+        int[] actual = twoSumII.twoSumByHashMap(new int[]{2,3,4}, 6);
+        Assert.assertArrayEquals(new int[]{1,3}, actual);
+    }
+
+    @Test
+    public void testTwoSumIICaseThree() {
+        TwoSumII twoSumII = new TwoSumII();
+        int[] actual = twoSumII.twoSumByHashMap(new int[]{-1,0}, -1);
+        Assert.assertArrayEquals(new int[]{1,2}, actual);
+    }
+
+    @Test
+    public void testTwoSumIICaseFour() {
+        TwoSumII twoSumII = new TwoSumII();
+        int[] actual = twoSumII.twoSumByHashMap(new int[]{0,0,3,4}, 0);
+        Assert.assertArrayEquals(new int[]{1,2}, actual);
+    }
+}
