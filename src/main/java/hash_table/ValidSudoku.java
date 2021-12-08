2bd2b493f9be7e22bb787a5f3ab029892dec7704
Соловьев Илья Владимирович (4012941) <isolovev@dev.vtb.ru>
2021-12-08 15:11:46 +0400

    Added complexity

--- src/main/java/hash_table/ValidSudoku.java
+++ src/main/java/hash_table/ValidSudoku.java
@@ -52,18 +52,20 @@ import java.util.Set;
  * board[i][j] is a digit 1-9 or '.'.
  */
 
+// Time = O(9*9 = 81)
+// Time = O(9*9 = 81)
 public class ValidSudoku {
 
     public boolean isValidSudoku(char[][] board) {
 
         Set<String> seen = new HashSet<>();
-        for (int i=0; i<9; ++i) {
-            for (int j=0; j<9; ++j) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
                 char number = board[i][j];
                 if (number != '.') {
-                if (!seen.add(number + " in row " + i) ||
-                        !seen.add(number + " in column " + j) ||
-                        !seen.add(number + " in block " + i/3 + "-" + j/3))
+                if (!seen.add(number + " found in row " + i) ||
+                        !seen.add(number + " found in column " + j) ||
+                        !seen.add(number + " found in box " + i/3 + "-" + j/3))
                     return false;
                 }
             }
