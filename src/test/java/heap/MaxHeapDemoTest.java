b9e314b792cb0f2a435db771198d26d2d0d8bda5
Соловьев Илья Владимирович (4012941) <isolovev@dev.vtb.ru>
2021-12-27 12:26:24 +0400

    Min and Max Heap Demo

---
+++ src/test/java/heap/MaxHeapDemoTest.java
@@ -0,0 +1,12 @@
+package heap;
+
+import org.junit.Test;
+
+public class MaxHeapDemoTest {
+
+    @Test
+    public void caseOne() {
+        MaxHeapDemo test = new MaxHeapDemo();
+        test.maxHeapDemo();
+    }
+}
---
+++ src/test/java/heap/MinHeapDemoTest.java
@@ -0,0 +1,12 @@
+package heap;
+
+import org.junit.Test;
+
+public class MinHeapDemoTest {
+
+    @Test
+    public void caseOne() {
+        MinHeapDemo test = new MinHeapDemo();
+        test.minHeapDemo();
+    }
+}
