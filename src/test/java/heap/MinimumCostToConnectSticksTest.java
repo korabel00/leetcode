84991531385e206e0cee282a4018c05eb4a1723f
Соловьев Илья Владимирович (4012941) <isolovev@dev.vtb.ru>
2022-01-19 16:23:42 +0400

    MinimumCostToConnectSticks

---
+++ src/test/java/heap/MinimumCostToConnectSticksTest.java
@@ -0,0 +1,21 @@
+package heap;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MinimumCostToConnectSticksTest {
+
+    @Test
+    public void caseOne() {
+        MinimumCostToConnectSticks test = new MinimumCostToConnectSticks();
+        int actual = test.connectSticks(new int[]{2,4,3});
+        Assert.assertEquals(14, actual);
+    }
+
+    @Test
+    public void caseTwo() {
+        MinimumCostToConnectSticks test = new MinimumCostToConnectSticks();
+        int actual = test.connectSticks(new int[]{1,8,3,5});
+        Assert.assertEquals(30, actual);
+    }
+}
